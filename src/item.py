8171932f4fbef4e832e38374fd38777aaa69fb0b
LudaKend <Ludakend@gmail.com>
2023-08-22 16:48:09 +0300

    версия 1 к уроку 13.1

--- src/item.py
+++ src/item.py
@@ -1,30 +1,30 @@
+# класс Item содержит атрибуты:
+#- название товара: name
+#- цена за единицу товара: price
+#- количество товара в магазине: quantity
+
 class Item:
-    """
-    Класс для представления товара в магазине.
-    """
-    pay_rate = 1.0
+    pay_rate = 1
     all = []
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+#        self.discount_price = price * pay_rate
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
+        Item.all.append(self)
 
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
-        pass
 
-    def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
+    def __repr__(self):
+        return f"Item({self.name},{self.price}, {self.quantity})"
 
-        :return: Общая стоимость товара.
-        """
-        pass
+    def calculate_total_price(self):
+        '''получает общую стоимость конкретного товара в магазине'''
+        return self.price * self.quantity
 
-    def apply_discount(self) -> None:
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
-        pass
+    def apply_discount(self):
+        '''применяет установленную скидку для конкретного товара'''
+        self.price = self.price * self.pay_rate   #такой синтаксис для конкретного экземпляра!
+        return self.price
+
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+import pytest
+from src.Item import Item
+
+@pytest.fixture
+def item_test():
+    return Item("Мышь", 1000, 1000)
+
+
+def test_item_calculate_total_price(item_test):
+    '''тестируем метод подсчета общей стоимости'''
+
+    assert item_test.calculate_total_price() == 1000000
+
+def test_item_apply_discount(item_test):
+    '''тестируем применение скидки для конкретного товара'''
+
+    assert item_test.apply_discount() == 1000
