65d8495d5ece5fdee5b6a1a7463ffec8b5d54957
LudaKend <Ludakend@gmail.com>
2023-08-26 17:15:29 +0300

    версия 2 к уроку 13.2

--- src/Item.py
+++ src/Item.py
@@ -3,20 +3,24 @@
 #- цена за единицу товара: price
 #- количество товара в магазине: quantity
 
+#импортирую модуль csv, позволяющий работать с текстовым файлом с разделителями, у которого расширение .csv
+import csv
+
+
 class Item:
     pay_rate = 1
     all = []
     def __init__(self, name, price, quantity):
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 #        self.discount_price = price * pay_rate
-
-        Item.all.append(self)
+# добавление новых экземпляров (из прошлого Д/З закомментируем, потому что экземпляры будем создавать из csv-файла
+#        Item.all.append(self)
 
 
     def __repr__(self):
-        return f"Item({self.name},{self.price}, {self.quantity})"
+        return f"Item({self.__name},{self.price}, {self.quantity})"
 
     def calculate_total_price(self):
         '''получает общую стоимость конкретного товара в магазине'''
@@ -27,4 +31,33 @@ class Item:
         self.price = self.price * self.pay_rate   #такой синтаксис для конкретного экземпляра!
         return self.price
 
+
+    @property
+    def name(self):
+        '''создаю сеттер для приватного атрибута __name с помощью декоратора property'''
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        '''прописываю геттер для приватного атрибута __name с помощью декоратора property, сразу обрезаю'''
+        self.__name = name[:10]
+
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        ''' класс-метод, инициализирующий экземпляры класса Item данными из файла src/items.csv'''
+        cls.all.clear()
+ #       print(Item.all)         # для отладки
+        with open('../src/items.csv') as f:
+            data_file = csv.DictReader(f)
+            for line in data_file:
+                item1 = (cls(line['name'], line['price'], line['quantity']))
+ #               print(item1)  # для отладки
+                cls.all.append(item1)
+ #           print(len(Item.all))  # для отладки
+
+    @staticmethod
+    def string_to_number(item_string):
+        item_string = float(item_string)
+        return int(item_string)
 
